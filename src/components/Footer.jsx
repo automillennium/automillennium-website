f7f0d31dbba34dbfa71bf16624944ec1d75a81d0
gokul <gokul.k@automillennium.com>
2025-09-29 09:46:05 +0400

    version v1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -100,12 +100,7 @@ const Footer = () => {
           Privacy Policy
         </a>
       </div> */}
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/automillennium/automillennium-website.git
-git push -u origin main
+
 
     </footer>
   );
